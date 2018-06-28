82d3191cc9cea23a4c4493be09364dee69ceac76
T Law <redirect@gmail.com>
2018-06-28 19:04:59 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -48,6 +48,7 @@ def turn(board)
   index = input_to_index(input)
   if valid_move?(board, index) == true 
     move(board, index, char = "X")
+    display_board(board)
   else
     turn(board)
   end
