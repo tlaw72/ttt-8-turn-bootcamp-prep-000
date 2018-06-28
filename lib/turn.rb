5488f9f8addad83f6d9da712f5cef9afdc7e7140
T Law <redirect@gmail.com>
2018-06-28 18:50:47 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -33,6 +33,14 @@ def position_taken?(board, index)
 end
 
 
+# code your #valid_move? method here
+
+def valid_move?(board, index)
+  if index.between?(0,8) && position_taken?(board, index) == false
+    return true
+  end
+end
+
 
 def turn 
   
